39fa791596dd7142094372551b16de63dd9139e7
Shawn <1997mwb@gmail.com>
2022-04-05 15:59:46 +0800

    1.3 保留最后N个元素

--- Ch1_数据结构和算法/1_8_字典的运算_最大最小等.py
+++ Ch1_数据结构和算法/1_8_字典的运算_最大最小等.py
@@ -3,9 +3,37 @@
 prices = {
     'ACME': 45.23,
     'AAPL': 612.78,
-    'IBM':  205.55,
-    'HPQ':  37.20,
-    'FB':   10.75
+    'IBM': 205.55,
+    'HPQ': 37.20,
+    'FB': 10.75
 }
 
+print(prices['ACME'])
+
 # 取max或者min的value
+min_price = min(zip(prices.values(), prices.keys()))
+print(min_price)
+
+max_price = max(zip(prices.values(), prices.keys()))
+print(max_price)
+
+# 对字典进行排序
+prices_sorted = sorted(zip(prices.values(), prices.keys()))
+print(prices_sorted)
+
+# zip() 只能访问一次
+price_and_name = zip(prices.values(), prices.keys())
+print(min(price_and_name))
+# print(max(price_and_name
+
+
+# 直接对字典进行运算，实际上是对key进行操作
+print(min(prices))
+print(max(prices))
+
+# 根据value筛选对应的key
+print(min(prices, key=lambda k: prices[k]))
+print(max(prices, key=lambda k: prices[k]))
+# 如果需要最小值的value，需要额外进行操作
+min_value = prices[min(prices, key= lambda k : prices[k])]
+print(min_value)
