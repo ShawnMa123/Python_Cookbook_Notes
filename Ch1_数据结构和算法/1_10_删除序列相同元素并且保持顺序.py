84b1bce7d9597cf6b85363fa191528300a4847c6
Shawn <1997mwb@gmail.com>
2022-04-17 19:47:36 +0800

    add 1.10

--- Ch1_数据结构和算法/1_10_删除序列相同元素并且保持顺序.py
+++ Ch1_数据结构和算法/1_10_删除序列相同元素并且保持顺序.py
@@ -9,3 +9,22 @@ def dedupe(items):
 
 a = [1, 5, 2, 1, 9, 1, 5, 10]
 print(list(dedupe(a)))
+
+
+# 假如元素是不可哈希的 例如dict
+def dedupe(items, key=None):
+    seen = set()
+    for item in items:
+        val = item if key is None else key(item)
+        if val not in seen:
+            yield item
+            seen.add(val)
+
+
+a = [{'x': 1, 'y': 2}, {'x': 1, 'y': 3}, {'x': 1, 'y': 2}, {'x': 2, 'y': 4}]
+# 将dict的key和value都作为去重的filter
+res = list(dedupe(a, key=lambda d: (d['x'], d['y'])))
+print(res)
+# 将dict的key作为去重的filter
+res = list(dedupe(a, key=lambda d: (d['x'])))
+print(res)
